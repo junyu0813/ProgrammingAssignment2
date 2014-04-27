5be0d137631644408c548e314ad8b9da492c5cfc
junyu0813 <junyu0813@gmail.com>
2014-04-26 23:24:25 -0400

    Second submission of ProgrammingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,5 @@
+## Matrix inversion is usually a costly computation and their may be some benefit to caching ## the inverse of a matrix rather than compute it repeatedly. The following a pair of ## functions will cache the inverse of a matrix therefore save a lot of computation.
+
 ## makeCacheMatrix function creates a special "matrix" 
 ## object that can cache its inverse.
 
